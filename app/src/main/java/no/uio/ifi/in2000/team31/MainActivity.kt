4bc5300a67daaf679f6f63f30ab4b153d3b9bead
Emil Øgård <emilog@uio.no>
2024-05-08 01:02:59 +0200

    kvelden

--- app/src/main/java/no/uio/ifi/in2000/team31/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team31/MainActivity.kt
@@ -48,8 +48,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
-                    SetupNavGraph(navController, homeViewModel, settingsViewModel)
+                    //val navController = rememberNavController()
+                    //SetupNavGraph(navController, homeViewModel, settingsViewModel)
+                    MoodApp(
+                        homeViewModel = homeViewModel,
+                        settingsViewModel = settingsViewModel,
+                    )
                 }
             }
         }
