597aa6fa17dc3103b6d0865d47bac587aeb7be94
Sebastian Hoxhallari <sebastian.hoxhallari@gmail.com>
2024-05-12 04:35:27 +0200

    Minor bug fix in addActivityViewModel

--- app/src/main/java/no/uio/ifi/in2000/team31/ui/activity/AddActivityViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team31/ui/activity/AddActivityViewModel.kt
@@ -1,14 +1,20 @@
 package no.uio.ifi.in2000.team31.ui.activity
 
+import android.app.Application
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import no.uio.ifi.in2000.team31.MoodApplication
 import no.uio.ifi.in2000.team31.data.activity.Activity
 import no.uio.ifi.in2000.team31.data.activity.ActivityRepository
 import no.uio.ifi.in2000.team31.ui.mood.Mood
 
-class AddActivityViewModel(private val activityRepository: ActivityRepository) : ViewModel() {
+class AddActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val appContainer = (application as MoodApplication).appContainer
+    private val activityRepository: ActivityRepository = appContainer.activityRepository
     var activityUiState by mutableStateOf(ActivityUiState())
         private set
 
