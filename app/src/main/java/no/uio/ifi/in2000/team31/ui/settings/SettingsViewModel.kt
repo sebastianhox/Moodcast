ec0158257fcf3e6c37d36565390cfe0b916c01e0
Sebastian Hoxhallari <sebastian.hoxhallari@gmail.com>
2024-05-13 15:11:37 +0200

    Major fixes in managing location permissions

--- app/src/main/java/no/uio/ifi/in2000/team31/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team31/ui/settings/SettingsViewModel.kt
@@ -2,7 +2,9 @@ package no.uio.ifi.in2000.team31.ui.settings
 
 import android.app.Application
 import android.content.Context
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
@@ -12,7 +14,9 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import no.uio.ifi.in2000.team31.MainActivity
 import no.uio.ifi.in2000.team31.MoodApplication
+import no.uio.ifi.in2000.team31.cache.CachePolicy
 
 
 /*enum class AppTheme {
@@ -26,6 +30,7 @@ object SettingsDataStore {
     val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
     val DARK_THEME_ON_KEY = booleanPreferencesKey("dark_theme_on")
     val FAHRENHEIT_ON = booleanPreferencesKey("fahrenheit_on")
+    val LOCATION_ON = booleanPreferencesKey("location_on")
 }
 
 class SettingsViewModel(application: Application) : AndroidViewModel(application) {
@@ -39,6 +44,9 @@ class SettingsViewModel(application: Application) : AndroidViewModel(application
     private val _isFahrenheit = MutableStateFlow(false)
     val isFahrenheit = _isFahrenheit.asStateFlow()
 
+    private val _locationOn = MutableStateFlow(false)
+    val locationOn = _locationOn.asStateFlow()
+
     init { // Setter theme til det som er lagret i repo
         viewModelScope.launch {
             Log.d("settings", "init SettingsViewModel")
@@ -48,6 +56,9 @@ class SettingsViewModel(application: Application) : AndroidViewModel(application
 
             val fahrenheitOn = settingsRepository.getFahrenheitSwitchState()
             _isFahrenheit.value = fahrenheitOn
+
+            val locationOn = settingsRepository.getLocationSwitchState()
+            _locationOn.value = locationOn
         }
     }
 
@@ -63,6 +74,22 @@ class SettingsViewModel(application: Application) : AndroidViewModel(application
             settingsRepository.updateFahrenheitSwitchState(isChecked)
             _isFahrenheit.value = settingsRepository.getFahrenheitSwitchState()
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun onLocationSwitchChange(isChecked: Boolean) {
+        viewModelScope.launch {
+            settingsRepository.updateLocationSwitchState(isChecked) // Oppdaterer repo med ny verdi
+            _locationOn.value = settingsRepository.getLocationSwitchState() // Endrer på verdien slik at composable oppdaterer
+
+            if (_locationOn.value) {
+                Log.d("settingss", "location on")
+                MainActivity.getInstance()?.requestLocationAndStartUpdates(CachePolicy(CachePolicy.Type.ALWAYS))
+            }
+
+        }
+
+
     }
 
 
